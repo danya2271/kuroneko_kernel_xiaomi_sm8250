11bb1bf97212eb32cfd4e7d987bc90fb7075bbd8
idkwhoiam322 <idkwhoiam322@raphielgang.org>
2020-02-09 00:56:25 +0530

    suspend: Use s2idle by default over deep
    Acording to google, this reduces latency during suspend/resume.
    
    Signed-off-by: idkwhoiam322 <idkwhoiam322@raphielgang.org>
    Signed-off-by: Lau <laststandrighthere@gmail.com>
    Signed-off-by: GeoPD <geoemmanuelpd2001@gmail.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -760,17 +760,17 @@ static int sugov_init(struct cpufreq_policy *policy)
 
 	if (cpumask_test_cpu(policy->cpu, cpu_lp_mask)) {
 		tunables->up_rate_limit_us = 5000;
-		tunables->down_rate_limit_us = 5000;
+		tunables->down_rate_limit_us = 2500;
 	}
 
 	if (cpumask_test_cpu(policy->cpu, cpu_perf_mask)) {
-		tunables->up_rate_limit_us = 16000;
+		tunables->up_rate_limit_us = 12000;
 		tunables->down_rate_limit_us = 4000;
 	}
 
         if (cpumask_test_cpu(policy->cpu, cpu_prime_mask)) {
-                tunables->up_rate_limit_us = 16000;
-                tunables->down_rate_limit_us = 4000;
+                tunables->up_rate_limit_us = 31000;
+                tunables->down_rate_limit_us = 5000;
         }
 
 	policy->governor_data = sg_policy;

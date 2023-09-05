7f0719b952e33b7457f5f709203cc6abe8363c16
danya2271 <danya2271@yandex.ru>
2023-09-05 16:25:39 +0500

    Add wakelock to boeffla wl list

--- drivers/base/power/boeffla_wl_blocker.h
+++ drivers/base/power/boeffla_wl_blocker.h
@@ -16,8 +16,8 @@
 
 #define BOEFFLA_WL_BLOCKER_VERSION	"1.1.0"
 
-#define LIST_WL_DEFAULT				"qcom_rx_wakelock;wlan;wlan_wow_wl;wlan_extscan_wl;netmgr_wl;NETLINK"
+#define LIST_WL_DEFAULT				"qcom_rx_wakelock;wlan;wlan_wow_wl;wlan_extscan_wl;netmgr_wl;NETLINK;[timerfd];elliptic_wake_source;smp2p-sleepstate;998000.qcom,qup_uart;hal_bluetooth_lock;event2;spi1.0;c440000.qcom,spmi:qcom,pm8150b@2:qcom,qpnp-smb5"
 
 #define LENGTH_LIST_WL				255
-#define LENGTH_LIST_WL_DEFAULT		100
+#define LENGTH_LIST_WL_DEFAULT		1000
 #define LENGTH_LIST_WL_SEARCH		LENGTH_LIST_WL + LENGTH_LIST_WL_DEFAULT + 5

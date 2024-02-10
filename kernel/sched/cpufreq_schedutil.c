104a2ef46b8601fb5942ee8e18b691d1e7f7f16b
danya2271 <danya2271@.yandex.ru>
2024-02-10 20:53:16 +0500

    Meow

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -772,8 +772,8 @@ static int sugov_init(struct cpufreq_policy *policy)
 	}
 
 	if (cpumask_test_cpu(policy->cpu, cpu_perf_mask)) {
-		tunables->up_rate_limit_us = 12000;
-		tunables->down_rate_limit_us = 4000;
+		tunables->up_rate_limit_us = 6500;
+		tunables->down_rate_limit_us = 1000;
 	}
 
         if (cpumask_test_cpu(policy->cpu, cpu_prime_mask)) {

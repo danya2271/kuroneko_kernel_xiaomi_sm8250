2a5f6d4465f7b32d66ca9034d743ebf27d773a08
danya2271 <danya2271@.yandex.ru>
2024-02-23 13:41:11 +0500

    treewide: remove some debugging

--- drivers/soc/qcom/msm_performance.c
+++ drivers/soc/qcom/msm_performance.c
@@ -128,12 +128,6 @@ static int get_cpu_min_freq(char *buf, const struct kernel_param *kp)
 	return cnt;
 }
 
-static const struct kernel_param_ops param_ops_cpu_min_freq = {
-	.set = set_cpu_min_freq,
-	.get = get_cpu_min_freq,
-};
-module_param_cb(cpu_min_freq, &param_ops_cpu_min_freq, NULL, 0644);
-
 static int set_cpu_max_freq(const char *buf, const struct kernel_param *kp)
 {
 #if 0

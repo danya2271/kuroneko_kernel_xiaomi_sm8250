2459c2bbfa7c6398367830b364c77845ed639f04
danya2271 <danya2271@.yandex.ru>
2024-03-06 23:50:37 +0500

    Meow

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -767,18 +767,18 @@ static int sugov_init(struct cpufreq_policy *policy)
 	tunables->down_rate_limit_us = cpufreq_policy_transition_delay_us(policy);
 
 	if (cpumask_test_cpu(policy->cpu, cpu_lp_mask)) {
-		tunables->up_rate_limit_us = 5000;
+		tunables->up_rate_limit_us = 5500;
 		tunables->down_rate_limit_us = 2500;
 	}
 
 	if (cpumask_test_cpu(policy->cpu, cpu_perf_mask)) {
-		tunables->up_rate_limit_us = 6500;
-		tunables->down_rate_limit_us = 1000;
+		tunables->up_rate_limit_us = 8500;
+		tunables->down_rate_limit_us = 500;
 	}
 
         if (cpumask_test_cpu(policy->cpu, cpu_prime_mask)) {
-                tunables->up_rate_limit_us = 31000;
-                tunables->down_rate_limit_us = 5000;
+                tunables->up_rate_limit_us = 45000;
+                tunables->down_rate_limit_us = 2000;
         }
 
 	policy->governor_data = sg_policy;

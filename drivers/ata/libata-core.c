6262c527f7e8a45504b0b4e731c6a1331779b7e6
danya2271 <danya2271@.yandex.ru>
2024-03-09 20:12:51 +0500

    Revert "ata: libata-core: Disable READ LOG DMA EXT for Samsung 840 EVOs"
    This reverts commit f6691be2b537bace468fd4cbe218bb8682107127.

--- drivers/ata/libata-core.c
+++ drivers/ata/libata-core.c
@@ -4598,9 +4598,6 @@ static const struct ata_blacklist_entry ata_device_blacklist [] = {
 						ATA_HORKAGE_ZERO_AFTER_TRIM, },
 	{ "Crucial_CT*MX100*",		"MU01",	ATA_HORKAGE_NO_NCQ_TRIM |
 						ATA_HORKAGE_ZERO_AFTER_TRIM, },
-	{ "Samsung SSD 840 EVO*",	NULL,	ATA_HORKAGE_NO_NCQ_TRIM |
-						ATA_HORKAGE_NO_DMA_LOG |
-						ATA_HORKAGE_ZERO_AFTER_TRIM, },
 	{ "Samsung SSD 840*",		NULL,	ATA_HORKAGE_NO_NCQ_TRIM |
 						ATA_HORKAGE_ZERO_AFTER_TRIM, },
 	{ "Samsung SSD 850*",		NULL,	ATA_HORKAGE_NO_NCQ_TRIM |
